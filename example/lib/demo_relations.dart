9e43488c6645255e1bd5bebf51111f87aaa236e5
Vyacheslav Pak <VVPAK14@gmail.com>
2021-09-30 16:05:13 +0600

    Example fix and format fix

--- example/lib/demo_relations.dart
+++ example/lib/demo_relations.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide TextEditingAction;
 import 'package:relation/relation.dart';
 
 // ignore_for_file: avoid-returning-widgets
